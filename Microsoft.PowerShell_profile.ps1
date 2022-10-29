ed4e6335e20d0510c10c83b68215655b73c654db
zefiro <jantoniofgonzalez20@gmail.com>
2022-10-29 13:07:06 +0200

    Profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\dracula.omp.json" | Invoke-Expression
+
+
+"
+             _.-;;-._
+      '-..-'|   ||   |      
+      '-..-'|_.-;;-._|
+      '-..-'|   ||   |
+      '-..-'|_.-''-._|
+
+
+"
+
+Import-Module -Name Terminal-Icons
+
+ 
