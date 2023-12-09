1df117d1a9a6ea4ccecf326b108f2c1ea5082506
janto <jantoniofgonzalez20@gmail.com>
2023-12-09 21:11:32 +0100

    modificado el profile @

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,10 @@
+
+
+powershell -executionpolicy bypass -File "C:\Users\janto\OneDrive\Documentos\PowerShell\Scripts\winfetch.ps1"
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\zash.omp.json" | Invoke-Expression
-
-
-"
-             _.-;;-._
-      '-..-'|   ||   |      
-      '-..-'|_.-;;-._|
-      '-..-'|   ||   |
-      '-..-'|_.-''-._|
-
-
-"
 
 Import-Module -Name Terminal-Icons
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
  
+Set-Alias lvim 'C:\Users\janto\.local\bin\lvim.ps1'
